43608cd1108a11e78f3d7bfb1a8b5cb1577c8990
shubham1507 <shubham.joshi1507@gmail.com>
2022-05-23 17:03:24 +0530

    first commit

---
+++ src/com/github/mattamorphic/registry/stub/Foo.java
@@ -0,0 +1,18 @@
+package dryrun.github.mattamorphic.registry.stub;
+
+public class Foo {
+
+    /**
+     * Example static method
+     */
+    public static void hello() {
+	    System.out.println("Hello, World!");
+    }
+
+    /**
+     * Example static method
+     */
+    public static void goodbye() {
+        System.out.println("Goodbye, World!");
+    }
+}
---
+++ src/example/main.java
@@ -0,0 +1,10 @@
+package example;
+
+import dryrun.github.mattamorphic.registry.stub.Foo;
+
+class Main {
+    public static void main(String[] args) {
+        Foo.hello();
+        Foo.goodbye();
+    }
+}
